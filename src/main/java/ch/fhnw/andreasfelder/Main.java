59b8701a77645df71e6bb1e3c747a2b343a5c7cd
Andreas Felder <andreas.felder@students.fhnw.ch>
2024-10-28 09:31:20 +0100

    add triangle rasterization

--- src/main/java/ch/fhnw/andreasfelder/Main.java
+++ src/main/java/ch/fhnw/andreasfelder/Main.java
@@ -1,6 +1,7 @@
 package ch.fhnw.andreasfelder;
 
 import ch.fhnw.andreasfelder.helpers.vertexColor;
+import ch.fhnw.andreasfelder.vector.Matrix3x2;
 import ch.fhnw.andreasfelder.vector.Matrix4x4;
 import ch.fhnw.andreasfelder.vector.MeshGenerator;
 import ch.fhnw.andreasfelder.vector.Tri;
@@ -13,6 +14,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,18 +66,26 @@ public class Main extends JPanel {
     @Override
     public void paint(Graphics g) {
         super.paint(g);
-        //Skip if MVP is null -> not yet calculated / initialized
+
         if (MVP == null) {
             return;
         }
+
+        int width = getWidth();
+        int height = getHeight();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        float w_half = width / 2f;
+        float h_half = height / 2f;
+
         for (Tri t : tri) {
             Vertex v1 = projectVertex(VertexShader(vertices.get(t.a()), MVP));
             Vertex v2 = projectVertex(VertexShader(vertices.get(t.b()), MVP));
             Vertex v3 = projectVertex(VertexShader(vertices.get(t.c()), MVP));
 
-            Vector2 p1 = transformToScreenPixel(v1, getWidth() / 2f, getHeight() / 2f);
-            Vector2 p2 = transformToScreenPixel(v2, getWidth() / 2f, getHeight() / 2f);
-            Vector2 p3 = transformToScreenPixel(v3, getWidth() / 2f, getHeight() / 2f);
+            Vector2 p1 = transformToScreenPixel(v1, w_half, h_half);
+            Vector2 p2 = transformToScreenPixel(v2, w_half, h_half);
+            Vector2 p3 = transformToScreenPixel(v3, w_half, h_half);
 
             Vector3 AB = new Vector3(p2.x() - p1.x(), p2.y() - p1.y(), 0);
             Vector3 AC = new Vector3(p3.x() - p1.x(), p3.y() - p1.y(), 0);
@@ -84,12 +94,33 @@ public class Main extends JPanel {
                 continue;
             }
 
-            g.setColor(v1.color().awtColorFromVector());
-            g.fillPolygon(
-                new int[] {(int) p1.x(), (int) p2.x(), (int) p3.x()},
-                new int[] {(int) p1.y(), (int) p2.y(), (int) p3.y()},
-                3);
+            final Vector2 ABScreenPixel = p2.subtract(p1);
+            final Vector2 ACScreenPixel = p3.subtract(p1);
+            final Matrix3x2 ABACInv = Matrix3x2.invert(new Matrix3x2(ABScreenPixel.x(), ABScreenPixel.y(), ACScreenPixel.x(), ACScreenPixel.y(), 0, 0));
+
+            if (ABACInv == null) {
+                continue;
+            }
+
+            float m11 = ABACInv.m11();
+            float m21 = ABACInv.m21();
+            float m12 = ABACInv.m12();
+            float m22 = ABACInv.m22();
+            int v1Color = v1.color().awtColorFromVector().getRGB();
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    final Vector2 AP = new Vector2(x + 0.25f, y + 0.25f).subtract(p1);
+                    final float u = m11 * AP.x() + m21 * AP.y();
+                    final float v = m12 * AP.x() + m22 * AP.y();
+
+                    if (u >= 0 && v >= 0 && (u + v) < 1) {
+                        image.setRGB(x, y, v1Color);
+                    }
+                }
+            }
         }
+        g.drawImage(image, 0, 0, null);
     }
 
     Vertex VertexShader(Vertex v, Matrix4x4 MVP) {
